b185cf738873e5c972d678b7d0f71cb21c57db9f
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-03-19 00:34:22 +0200

    shuffle answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,7 +4,11 @@ import QUESTIONS from "../questions.js";
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
+
     const activeQuestionIndex = userAnswers.length;
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+
+    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswer) => {
@@ -12,14 +16,16 @@ export default function Quiz() {
         });
     }
 
-    return <div id="question">
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">
-            {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-                <li key={answer} className="answer">
-                    <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                </li>
-            ))}
-        </ul>
+    return <div id="quiz">
+        <div id="question">
+            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+            <ul id="answers">
+                {shuffledAnswers.map((answer) => (
+                    <li key={answer} className="answer">
+                        <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
     </div>
 }
