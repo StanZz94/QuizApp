01d88a6713078fd512dc240ae897fdacf0eada8a
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-03-18 02:38:19 +0200

    render quizs and answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,25 @@
-import { useState } from "react"
+import { useState } from "react";
+
+import QUESTIONS from "../questions.js";
 
 export default function Quiz() {
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
     const [userAnswers, setUserAnswers] = useState([]);
+    const activeQuestionIndex = userAnswers.length;
 
-    return <p>Active Quiz</p>
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers((prevUserAnswer) => {
+            return [...prevUserAnswer, selectedAnswer];
+        });
+    }
+
+    return <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+            {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                <li key={answer} className="answer">
+                    <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                </li>
+            ))}
+        </ul>
+    </div>
 }
