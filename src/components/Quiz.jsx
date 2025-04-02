a81e1456548716255bc55f17e0818b9a11e3dfc4
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-04-03 00:20:20 +0300

    move state down and  transfer logic where is needed

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,33 +5,18 @@ import quizComplete from "../assets/quiz-complete.png"
 import Question from "./Question.jsx";
 
 export default function Quiz() {
-    const [answerState, setAnswerState] = useState('');
     const [userAnswers, setUserAnswers] = useState([]);
 
-    const activeQuestionIndex = answerState === "" ? userAnswers.length : userAnswers.length - 1;
+    const activeQuestionIndex = userAnswers.length;
 
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
-        setAnswerState("answered")
         setUserAnswers((prevUserAnswer) => {
             return [...prevUserAnswer, selectedAnswer];
         });
 
-        setTimeout(() => {
-            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-                setAnswerState('correct');
-            } else {
-                setAnswerState('wrong');
-            }
-
-            setTimeout(() => {
-                setAnswerState('');
-            }, 2000);
-
-        }, 1000);
-    },
-        [activeQuestionIndex]);
+    },[]);
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
@@ -45,10 +30,7 @@ export default function Quiz() {
     return <div id="quiz">
         <Question
             key={activeQuestionIndex}
-            questionText={QUESTIONS[activeQuestionIndex].text}
-            answerState={answerState}
-            answers={QUESTIONS[activeQuestionIndex].answers}
-            selectedAnswer={userAnswers[userAnswers.length - 1]}
+            index={activeQuestionIndex}
             onSelectAnswer={handleSelectAnswer}
             onSkipAnswer={handleSkipAnswer}
         />
