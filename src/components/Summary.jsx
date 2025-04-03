d8e80e85113cef361dd64558373be076fe99a8cb
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-04-03 04:12:10 +0300

    calculate results

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -3,6 +3,19 @@ import QUESTIONS from "../questions";
 
 export default function Summary({ userAnswers }) {
 
+    const skippedAnswers = userAnswers.filter((answer) => answer === null);
+    const correctAnswers = userAnswers.filter((answer, index) => answer === QUESTIONS[index].answers[0])
+
+    const skippedAnswersShare = Math.round(
+        (skippedAnswers.length / userAnswers.length) * 100
+    );
+
+    const correctAnswersShare = Math.round(
+        (correctAnswers.length / userAnswers.length) * 100
+    );
+
+    const wrongAnswersShare = 100 - skippedAnswersShare - correctAnswersShare;
+
     return (<div id="summary">
         <img src={quizComplete} alt='quiz complete' />
         <h2>Quiz Completed!</h2>
